774339bced3d8316ab878ab37182c10002d27d3a
Byron Wade <bcw1995@gmail.com>
2023-10-17 23:09:02 -0700

    more

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -1,10 +1,8 @@
 import Header from "@/components/sections/Header";
 import Footer from "@/components/sections/Footer";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react";
 import getMenu from "./getMenu";
 import Script from "next/script";
-import Head from "next/head";
 
 export default async function RootLayout({ children, props }: { children: React.ReactNode; props?: any }) {
 	const data = await getMenu();
@@ -24,7 +22,6 @@ export default async function RootLayout({ children, props }: { children: React.
 			<body className="bg-gray-50 text-base">
 				<Header data={data} />
 				{children}
-				<Analytics />
 				<Footer />
 				{/* <Script id="ze-snippet" strategy="lazyOnload" src="https://static.zdassets.com/ekr/snippet.js?key=06e45130-bfd2-4b2b-8137-28903b96f527"></Script> */}
 			</body>
