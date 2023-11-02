2839adfe6534d8563a10131ee86b69a07cce8416
Byron Wade <bcw1995@gmail.com>
2023-11-01 22:55:36 -0700

    Accessability

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,20 +1,73 @@
 import { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
-	return {
+    return {
 		name: "Wade's Plumbing and Septic",
 		short_name: "Wade's",
-		description: "Plumbing and septic company",
+		description: "Expert plumbing and septic services in Santa Cruz, California. Fast, reliable, and tech-forward solutions.",
 		start_url: "/",
 		display: "standalone",
-		background_color: "#fff",
-		theme_color: "#fff",
+		background_color: "#000",
+		theme_color: "#bc6f30", // You can change this to match your brand color
 		icons: [
 			{
-				src: "/favicon.ico",
-				sizes: "any",
-				type: "image/x-icon",
+				src: "assets/icons/icon-48x48.png",
+				sizes: "48x48",
+				type: "image/png",
+				purpose: "maskable",
+			},
+			{
+				src: "assets/icons/icon-72x72.png",
+				sizes: "72x72",
+				type: "image/png",
+				purpose: "maskable",
+			},
+			{
+				src: "assets/icons/icon-96x96.png",
+				sizes: "96x96",
+				type: "image/png",
+				purpose: "maskable",
+			},
+			{
+				src: "assets/icons/icon-128x128.png",
+				sizes: "128x128",
+				type: "image/png",
+				purpose: "maskable",
+			},
+			{
+				src: "assets/icons/icon-144x144.png",
+				sizes: "144x144",
+				type: "image/png",
+				purpose: "maskable",
+			},
+			{
+				src: "assets/icons/icon-152x152.png",
+				sizes: "152x152",
+				type: "image/png",
+				purpose: "maskable",
+			},
+			{
+				src: "assets/icons/icon-192x192.png",
+				sizes: "192x192",
+				type: "image/png",
+				purpose: "maskable",
+			},
+			{
+				src: "assets/icons/icon-384x384.png",
+				sizes: "384x384",
+				type: "image/png",
+				purpose: "maskable",
+			},
+			{
+				src: "assets/icons/icon-512x512.png",
+				sizes: "512x512",
+				type: "image/png",
+				purpose: "maskable",
 			},
 		],
+		// Optionally, you can include orientation, scope, and other properties
+		orientation: "portrait",
+		scope: "/",
+		lang: "en-US",
 	};
 }
