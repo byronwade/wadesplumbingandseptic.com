97a50c029c334b911c1b3db96d728d79793aad0a
Byron Wade <bcw1995@gmail.com>
2023-11-06 16:46:06 -0800

    landing pages

--- src/app/(website)/(landing-pages)/layout.tsx
+++ src/app/(website)/(landing-pages)/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 				<div className="relitive flex p-3 items-center justify-between mx-auto max-w-7xl px-6 lg:px-8">
 					<Link href="/" className="flex items-center space-x-4 hover:underline">
 						<Image className="w-auto h-auto" src="/WadesLogo.webp" alt="Wade's Plumbing & Septic Logo" width={40} height={40} />
-						<h1 className="font-bold text-2xl hidden md:inline-flex">Wades Plumbing &amp; Septic</h1>
+						<span className="font-bold text-2xl hidden md:inline-flex">Wades Plumbing &amp; Septic</span>
 					</Link>
 					<Link href="tel:18312254344" className="inline-flex justify-center rounded font-bold px-3.5 py-2.5 bg-brand text-black hover:bg-brand-600">
 						Call (831)-225-4344
--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -8,7 +8,7 @@ export function NavBar({ open }) {
 		<>
 			<Link className="flex items-center space-x-4" href="/">
 				<Image className="w-auto h-auto" src="/WadesLogo.webp" width={40} height={40} alt="Wade's Plumbing & Septic Logo" />
-				<h1 className="font-bold text-2xl hidden xl:inline-flex">Wades Plumbing & Septic</h1>
+				<span className="font-bold text-2xl hidden xl:inline-flex">Wades Plumbing & Septic</span>
 			</Link>
 			<div className="flex font-bold items-center md:hidden">
 				<Disclosure.Button className="inline-flex items-center justify-center rounded p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
